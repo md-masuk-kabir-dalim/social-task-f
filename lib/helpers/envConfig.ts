9e2d22e1947673e2c6cca27e8698bde06c3fda66
dalim-kazi <mddalim862483@gmail.com>
2026-04-05 21:19:41 +0600

    base url change

--- lib/helpers/envConfig.ts
+++ lib/helpers/envConfig.ts
@@ -1,5 +1,5 @@
 export const envConfig = {
-  baseApi: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8010",
+  baseApi: process.env.NEXT_PUBLIC_API_URL || "https://sk-api.gofast.business",
   api_key:
     process.env.NEXT_PUBLIC_API_KEY ||
     "VALID_API_KEYS_ySWQiOiIxMjM0NSIsImVtYWlsIjoidXNlckB",
